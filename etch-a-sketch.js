9684fb4083138136b24d85fa6b8bd92d74dd2dab
rosnbro <benro95@gmail.com>
2022-01-09 02:37:19 -0500

    refined style and simplified js logic

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,24 +7,23 @@ const shakeButton = document.querySelector(".shakeButton");
 const settings = document.querySelector(".settings");
 const blackButton = document.querySelector(".blackButton");
 const rainbowButton = document.querySelector(".rainbowButton");
+const colorPicker = document.querySelector(".colorButton");
 
-let color = ["black", "black", "black", "black", "black", "black", "black"];
+let color = [];
 let input = document.querySelector("#columns");
 let columns = input.value;
 
-setButton.addEventListener("click", () => sizePrompt());
+setButton.addEventListener("click", () => settingMenu());
 shakeButton.addEventListener("click", () => clearContainer());
-blackButton.addEventListener("click", () => {
-    color = ["black", "black", "black", "black", "black", "black", "black"]
-});
-rainbowButton.addEventListener("click", () => {
-    color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-});
+blackButton.addEventListener("click", () => black());
+rainbowButton.addEventListener("click", () => rainbow());
+colorPicker.addEventListener("input", () => customColor());
 input.addEventListener("input", () => {
     columns = input.value
     clearContainer()
 });
 
+black();
 displayGrid(columns);
 
 function displayGrid(columns) {
@@ -42,11 +41,42 @@ function displayGrid(columns) {
     }
 }
 
-function sizePrompt() {
-    //do {
-    //    columns = prompt("Resolution: Please input an integer from from 1 to 100.");
-    //} while (columns == null || columns <= 0 || columns > 100)
-    //clearContainer();
+function settingMenu() {
+    if (settings.style.visibility === "hidden") {
+        settings.style.visibility = "visible";
+    } else {
+        settings.style.visibility = "hidden";
+    }
+}
+
+function black() {
+    color = [];
+    for (let i = 0; i < 7; i++) {
+        color.push("black");
+    }
+    blackButton.classList.add("blackOn");
+    rainbowButton.classList.remove("rainbowOn");
+    colorPicker.value = "#b0e0e6";
+    colorPicker.style.backgroundColor = "#b0e0e6";
+}
+
+function rainbow() {
+    color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    rainbowButton.classList.add("rainbowOn");
+    blackButton.classList.remove("blackOn");
+    colorPicker.value = "#b0e0e6";
+    colorPicker.style.backgroundColor = "#b0e0e6";
+}
+
+function customColor() {
+    let pick = colorPicker.value;
+    color = [];
+    for (let i = 0; i < 7; i++) {
+        color.push(pick);
+    }
+    colorPicker.style.backgroundColor = pick;
+    rainbowButton.classList.remove("rainbowOn");
+    blackButton.classList.remove("blackOn");
 }
 
 function clearContainer() {
